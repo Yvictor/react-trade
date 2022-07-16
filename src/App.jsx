8d0e7eb2062303d9b28771318847adc9cd78326f
Yvictor <yvictor3141@gmail.com>
2022-07-16 20:57:11 +0800

    feat: setup app navbar and theme toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { Button, useTheme } from 'react-daisyui'
+import { Button, useTheme, Navbar, Swap } from 'react-daisyui'
+import { FiMoon, FiSun } from 'react-icons/fi'
+
+const ThemeButton = () => {
+  const { theme, setTheme } = useTheme()
+  const iconSize = 24
+  return (
+    <div className='p-1'>
+      <Swap onChange={() => setTheme((theme) => (theme === "light" ? "dark" : "light"))}
+        rotate={true} onElement={<FiMoon size={iconSize} />}
+        offElement={<FiSun size={iconSize} />}>
+      </Swap>
+    </div>
+
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-  const { theme, setTheme } = useTheme()
   return (
-    <div className="container mx-auto items-center">
-      <div className="flex">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <Button onClick={() => setTheme((theme) => (theme === "dark" ? "light" : "dark"))}>
-          theme {theme}
-        </Button>
+    <div className="w-screen h-screen">
+      <div className="flex h-full">
+        <div className='flex w-full'>
+          <div className='flex w-full h-16 bg-base-200 p-0 items-start'>
+            <Navbar>
+              <div className='flex-1'>
+                <p className='font-serif normal-case text-xl p-2'>ReactTrade</p>
+              </div>
+              <div className='ml-auto items-center p-1'>
+                <ThemeButton />
+              </div>
+            </Navbar>
+          </div>
+        </div>
       </div>
     </div>
   )
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["node_modules/daisyui/dist/**/*.js"],
+  content: [
+    "node_modules/daisyui/dist/**/*.js", 
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
